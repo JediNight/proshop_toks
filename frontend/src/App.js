b6c39a88a0e495407746cf260d564577d9199ce7
unknown <tafawibe@gmail.com>
2020-12-17 22:20:28 -0600

    First_Commit
    First ungit commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 import logo from "./logo.svg";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import "./App.css";
+import ProductScreen from "./screens/ProductScreen";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome to Proshop</h1>
-    </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 };
 export default App;
