2d5fe00799d9b0f091b7644a44f193f7032dff65
unknown <tafawibe@gmail.com>
2020-12-25 09:14:19 -0600

    BackEndUserAuthentication
    Added User Registration/Authentication/Authorization

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema(
   {
@@ -24,6 +25,19 @@ const userSchema = mongoose.Schema(
     timestamp: true,
   }
 );
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(this.password, salt);
+  this.password = await brcrypt.hash(this.password);
+});
 
 const User = mongoose.model("User", userSchema);
 
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import colors from "colors";
 import connectDB from "./config/db.js";
 
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
@@ -17,11 +19,14 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
 app.use("/api/products/", productRoutes);
+app.use("/api/users/", userRoutes);
 
 //Error middleware
 //Error for 404
